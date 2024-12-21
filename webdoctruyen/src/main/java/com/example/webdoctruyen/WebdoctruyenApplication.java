8634b981e0b28544e2f95ff8ea0b1641b38a699c
pck313 <23021596@vnu.edu.vn>
2024-12-21 10:16:01 +0700

    new

--- webdoctruyen/src/main/java/com/example/webdoctruyen/WebdoctruyenApplication.java
+++ webdoctruyen/src/main/java/com/example/webdoctruyen/WebdoctruyenApplication.java
@@ -2,12 +2,29 @@ package com.example.webdoctruyen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
+import java.awt.*;
+import java.net.URI;
 
 @SpringBootApplication
-public class WebdoctruyenApplication {
+public class WebdoctruyenApplication implements ApplicationListener<ApplicationReadyEvent> {
 
     public static void main(String[] args) {
         SpringApplication.run(WebdoctruyenApplication.class, args);
     }
 
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        openHomePage("http://localhost:8080");
+    }
+
+    public static void openHomePage(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
