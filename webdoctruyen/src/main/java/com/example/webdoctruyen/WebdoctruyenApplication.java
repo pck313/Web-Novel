c497b2668aeb919ec4eba9c8052d432f381404a6
pck313 <23021596@vnu.edu.vn>
2024-12-21 09:58:59 +0700

    .

--- webdoctruyen/src/main/java/com/example/webdoctruyen/WebdoctruyenApplication.java
+++ webdoctruyen/src/main/java/com/example/webdoctruyen/WebdoctruyenApplication.java
@@ -1,4 +1,4 @@
-package com.example.webdoctruyen;
+epackage com.example.webdoctruyen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
