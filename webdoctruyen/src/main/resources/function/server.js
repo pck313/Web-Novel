c732cf2a07338175cc939343dcf77da65e103bc0
pck313 <23021596@vnu.edu.vn>
2024-12-28 13:50:37 +0700

    .

--- webdoctruyen/src/main/resources/function/server.js
+++ webdoctruyen/src/main/resources/function/server.js
@@ -7,7 +7,7 @@ const port = 3000;
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'blackpinkinyourarea',
+    password: '',
     database: 'dbnovel'
 });
 
