bc2860b60f712f85212c32f7b69c4e590318f23a
pck313 <23021596@vnu.edu.vn>
2024-12-30 08:49:10 +0700

    genres

--- webdoctruyen/src/main/resources/function/genres.js
+++ webdoctruyen/src/main/resources/function/genres.js
@@ -8,20 +8,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
             genres.forEach(genre => {
                 const genreDiv = document.createElement('div');
                 genreDiv.classList.add('genre-item');
-                genreDiv.innerHTML = `<input type="checkbox" id="${genre}" name="genre" value="${genre}">
-                                      <label for="${genre}">${genre}</label>`;
+                // Tạo liên kết cho thể loại, truyền thể loại vào URL
+                genreDiv.innerHTML = `<a href="books.html?genre=${encodeURIComponent(genre)}">${genre}</a>`;
                 genresListDiv.appendChild(genreDiv);
             });
         })
         .catch(error => console.error('Error fetching data:', error));
 });
-
-function searchByGenres() {
-    const selectedGenres = Array.from(document.querySelectorAll('input[name="genre"]:checked')).map(checkbox => checkbox.value);
-    if (selectedGenres.length > 0) {
-        const queryString = selectedGenres.map(genre => `genre=${encodeURIComponent(genre)}`).join('&');
-        window.location.href = `search_results.html?${queryString}`;
-    } else {
-        alert('Vui lòng chọn ít nhất một thể loại!');
-    }
-}
